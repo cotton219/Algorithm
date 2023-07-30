64beec243b3f66f11b98af7d6ca50da41c29c0ee
cottonee <49609284+cotton219@users.noreply.github.com>
2023-07-30 19:54:51 +0900

    [Bronze III] Title: 최소, 최대, Time: 588 ms, Memory: 89600 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -1,26 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 	static int n, min, max;
-	static int[] arr;
+	static int x;
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		n = Integer.parseInt(br.readLine());
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		arr = new int[n];
 		
-		int i = 0;
+		min = 1000001;
+		max = -1000001;
+		
 		while(st.hasMoreTokens()) {
-			arr[i] = Integer.parseInt(st.nextToken());
-			i++;
+			x = Integer.parseInt(st.nextToken());
+			if (min > x) {
+				min = x;
+			}
+			if (max < x) {
+				max = x;
+			}
 		}
 		
-		Arrays.sort(arr);
-		System.out.println(arr[0] + " " + arr[n-1]);
+		System.out.println(min + " " + max);
 	}
 
 }
